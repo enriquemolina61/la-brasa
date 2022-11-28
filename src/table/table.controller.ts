84eaf4d1fd97b5cdad05e04a6df4597a67312005
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-11-28 19:51:28 -0300

    init swagger

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateTableDto } from './dto/createtable.dto';
 import { TableService } from './table.services';
 
+@ApiTags('Table')
 @Controller('table')
 export class TableController {
   constructor(private tableService: TableService) {}
