04f529e991815672b8c21dade3938b9f76821380
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-11-29 12:35:30 -0300

    generate product

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TableModule } from './table/table.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { ProductModule } from './product/product.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [TableModule, PrismaModule],
+  imports: [TableModule, PrismaModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
