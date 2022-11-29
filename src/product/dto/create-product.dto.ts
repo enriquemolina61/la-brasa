a9b38b93b656f66578ac7e19136f18ba3cd0c85b
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-11-29 14:32:06 -0300

    implement api-operation product

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do Produto',
+    example: 'Coca-Cola',
+  })
   name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Descrição do Produto',
+    example: 'Refrigerante de Cola',
+  })
   description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'Preço do Produto',
+    example: 5.5,
+  })
   price: number;
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do Produto',
+    example:
+      'https://tdc099.vtexassets.com/arquivos/ids/184011/FOTOSVTEX.jpg?v=637731910440870000',
+  })
   image: string;
 }
--- src/table/dto/updatetable.dto.ts
+++ src/table/dto/updatetable.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateTableDto } from './createtable.dto';
 
 export class UpdateTableDto extends PartialType(CreateTableDto) {}
